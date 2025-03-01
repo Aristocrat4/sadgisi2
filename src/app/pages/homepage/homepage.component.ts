c50e3c4098b15b12c631ec72b68c085d4a73126c
GDanelia <galaktion.danelia@space.ge>
2025-03-01 23:12:58 +0400

    add faq section

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroImagesComponent } from '../../components/hero-images/hero-images.component';
 import { RatingsComponent } from '../../shared/ratings/ratings.component';
+import { QuestionsComponent } from '../../shared/questions/questions.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [HeroImagesComponent, RatingsComponent],
+  imports: [HeroImagesComponent, RatingsComponent, QuestionsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
